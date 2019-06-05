8dc80bdcc4dea41562f644c96d8f387703bbec6d
Michael McCullough <michael.mccullough21@gmail.com>
2019-06-05 13:28:01 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,9 +1,9 @@
 board = ["", "", "", "", "", "", "", "", ""]
 
 def display_board(array)
-  puts "  #{array[0]} |,  #{array[1]} |,  #{array[2]}  "
+  puts " #{array[0]} | #{array[1]} | #{array[2]}  "
   puts "-----------"
-  puts "   |   |   "
+  puts " #{array[3]} | #{array[4]} | #{array[5]}  "
   puts "-----------"
   puts "   |   |   "
 end
